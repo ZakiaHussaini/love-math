bbfbadeb25d5c7d7983469894760000386bfef67
ZakiaHussaini <zakiahussaini19@gmail.com>
2022-08-09 06:44:58 +0000

    add event listener for document lading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+//wait for the DOM to finish loading before running the game
+// get the button elements and add event listener to them
+
+document.addEventListener('DOMContentLoaded',function(){
+    let buttons = document.getElementsByTagName('button');
+
+    for(let button of buttons){
+        button.addEventListener('click',function(){
+            if(this.getAttribute('data-type')==='submit'){
+                alert('you clicked sumbit');
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
+
+
 function runGame(){
 
 }
