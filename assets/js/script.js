1e0fa7580064e9650f4e35b41ab6dca42a0cb093
ZakiaHussaini <zakiahussaini19@gmail.com>
2022-08-09 11:25:59 +0000

    add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,9 @@ document.addEventListener('DOMContentLoaded',function(){
     } 
     else if(gameType==='multiply'){
         displayMultiplyQuestion(num1,num2);
+    } 
+    else if (gameType === 'subtract'){
+        displaySubtractQuestion(num1,num2);
     }
     
     else  {
@@ -79,6 +82,8 @@ function calculateCorrectAnswer(){
         return [operand1 + operand2, "addition"];
     } else if(operator==='x'){
         return[operand1 * operand2, 'multiply'];
+    } else if(operator = '-'){
+        return [operand1 - operand2, 'subtract'];
     }
 
     else {
@@ -109,8 +114,13 @@ document.getElementById('operand2').textContent = operand2;
 document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1,operand2) {
+document.getElementById('operand1').textContent = 
+operand1 > operand2 ? operand1 : operand2;
 
+document.getElementById('operand2').textContent = operand1 > operand2 ?
+operand2 : operand1;
+document.getElementById('operator').textContent = '-';
 }
 
 function displayMultiplyQuestion(operand1,operand2) {
