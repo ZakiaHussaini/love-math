019964a57e597850e7e516115b88c7fdafcfbc23
ZakiaHussaini <zakiahussaini19@gmail.com>
2022-08-17 03:47:25 +0000

    add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,9 @@ document.addEventListener('DOMContentLoaded',function(){
     else if (gameType === 'subtract'){
         displaySubtractQuestion(num1,num2);
     }
+    else if (gameType === 'division'){
+        displayDivisionQuestion(num1,num2);
+    }
     
     else  {
         alert(`Unknown game type: ${gameType}`);
@@ -88,8 +91,10 @@ function calculateCorrectAnswer(){
         return [operand1 + operand2, "addition"];
     } else if(operator==='x'){
         return[operand1 * operand2, 'multiply'];
-    } else if(operator = '-'){
+    } else if(operator === '-'){
         return [operand1 - operand2, 'subtract'];
+    } else if(operator ==='/'){
+        return[operand1 / operand2, 'division'];
     }
 
     else {
@@ -133,4 +138,16 @@ function displayMultiplyQuestion(operand1,operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+    
+   
+    
 }
